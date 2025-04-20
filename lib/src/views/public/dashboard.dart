cb049a516860277348a4821eec466e74497f69ef
shil-mil <shyshy122303@gmail.com>
2025-04-21 00:29:32 +0800

    Add sign out button

--- lib/src/views/public/dashboard.dart
+++ lib/src/views/public/dashboard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:univents/src/views/public/Sign_In_Page.dart';
+import 'package:univents/src/views/public/auth.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
@@ -7,9 +9,36 @@ class Dashboard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          "HomePage",
-        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("homepage"),
+            SizedBox(height: 30),
+            SizedBox(
+              height: 55,
+              width: 280,
+              child: ElevatedButton(
+                onPressed: () async {
+                  final user = await signOut();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Signed out successfully!"))
+                    );
+                      Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => SignInPage()),
+                    );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                child: Text("Sign out")
+              ),
+            ),
+          ],
+        )
       ),
     );
   }
