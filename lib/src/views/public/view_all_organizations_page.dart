56d4d32e0fbe1376090c234622142d0dbb4b634e
smm23 <shilohm.millondaga@gmail.com>
2025-05-03 01:18:24 +0800

    Create temporary "Create Organization" button

--- lib/src/views/public/view_all_organizations_page.dart
+++ lib/src/views/public/view_all_organizations_page.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:univents/src/views/customwidgets/organizations_cards.dart';
+import 'package:univents/src/views/public/create_organization.dart';
 
 class ViewAllOrganizationsPage extends StatefulWidget {
   const ViewAllOrganizationsPage({super.key, required List<OrganizationCard> organizations});
@@ -38,29 +39,53 @@ class _ViewAllOrganizationsPageState extends State<ViewAllOrganizationsPage> {
         ),
         backgroundColor: const Color(0xFF182C8C),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: FutureBuilder<List<OrganizationCard>>(
-          future: _futureOrganizationCardsCards,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(child: CircularProgressIndicator());
-            } else if (snapshot.hasError) {
-              return Center(child: Text('Error: ${snapshot.error}'));
-            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-              return const Center(child: Text('No organizations found.'));
-            } else {
-              final organizations = snapshot.data!;
-              return SingleChildScrollView(
-                child: Wrap(
-                  spacing: 15,
-                  runSpacing: 15,
-                  children: organizations,
-                ),
-              );
-            }
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: FutureBuilder<List<OrganizationCard>>(
+              future: _futureOrganizationCardsCards,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  return const Center(child: Text('No organizations found.'));
+                } else {
+                  final organizations = snapshot.data!;
+                  return SingleChildScrollView(
+                    child: Wrap(
+                      spacing: 15,
+                      runSpacing: 15,
+                      children: organizations,
+                    ),
+                  );
+                }
+              },
+            ),
+          ),
+          
+          ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.red,
+          ),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const CreateOrganization()),
+            );
           },
+          child: Text(
+            'Create Organization',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
         ),
+        ],
       ),
     );
   }
