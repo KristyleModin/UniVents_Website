3efce341432891b2256b1343cc38f886938a556d
smm23 <shilohm.millondaga@gmail.com>
2025-05-02 19:07:23 +0800

    Removed unused imports

--- lib/src/views/customwidgets/organizations_cards.dart
+++ lib/src/views/customwidgets/organizations_cards.dart
@@ -1,6 +1,4 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:univents/src/views/public/organization_details.dart';
 
 class OrganizationCard extends StatelessWidget {
